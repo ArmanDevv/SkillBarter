92b8a7e1c88b68494115dcfef8ecd1a948839154
armandevv <armansingh7827@gmail.com>
2025-02-07 10:50:04 +0530

    Commit Final

---
+++ Backend/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensures unique email
+  },
+  steps: {
+    type: Number,
+    required: true,
+    default: 0, // Default to 0 if no steps are provided
+  },
+  calories: {
+    type: Number,
+    required: true,
+    default: 0, // Default to 0 if no calories are provided
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
